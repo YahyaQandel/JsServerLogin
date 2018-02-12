c576f47116c9634350fb08d1e3ae933ae762e746
Yahya_beeieg <yahya.qandel@gmail.com>
2018-02-12 02:29:17 +0200

    initial commit & server is up

---
+++ index.js
@@ -0,0 +1,24 @@
+var express = require("express");
+var config  = require('./config')
+var port = config.port;
+var server_url = config.server_url
+var app = express();
+
+
+app.get("/login", function(req, res) {
+	res.header('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', '*');;
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+
+  var test = (req.query.username==config.SUCCEED_TEST_USERNAME&&req.query.password==config.SUCCEED_TEST_PASSWORD)
+
+  console.log(req.query);
+  res.send(test);
+});
+
+app.listen(port);
+console.log("Server is Up on "+server_url)
